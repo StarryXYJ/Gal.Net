afc108149c719968fb0fd872568a707d34733f13
sakuya <xiao.yejun666@gmail.com>
2026-07-04 22:03:32 +0800

    init

---
+++ src/Launcher/GalNet.Launcher/GalNet.Launcher/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GalNet.Launcher.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ src/Launcher/GalNet.Launcher/GalNet.Launcher/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GalNet.Launcher.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
